39ad40d168040bc8ad9d5f94cd731f0da8522e87
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-02-08 10:10:40 +0100

    Category and Brand filters added to Product viewset.

--- server/apps/product/logic/filters.py
+++ server/apps/product/logic/filters.py
@@ -6,6 +6,12 @@ from server.apps.product.models import Product
 class ProductFilter(filters.FilterSet):
     """Filter for Product model."""
 
+    category = filters.CharFilter(method="filter_category")
+    brand = filters.CharFilter(field_name="brand__slug", lookup_expr="iexact")
+
+    category_name = filters.CharFilter(field_name="category__name", lookup_expr="icontains")
+    brand_name = filters.CharFilter(field_name="brand__name", lookup_expr="icontains")
+
     min_price = filters.NumberFilter(field_name="price", lookup_expr="gte")
     max_price = filters.NumberFilter(field_name="price", lookup_expr="lte")
     discount = filters.BooleanFilter(field_name="discount", lookup_expr="gt")
@@ -16,9 +22,22 @@ class ProductFilter(filters.FilterSet):
     class Meta:
         model = Product
         fields = [
+            "category",
+            "brand",
+            "category_name",
+            "brand_name",
             "min_price",
             "max_price",
             "discount",
             "in_stock",
             "is_distributer",
         ]
+
+    def filter_category(self, queryset, name, value):
+        """Filter by category slug."""
+
+        result = queryset.filter(category__slug__iexact=value)
+        result = result.union(queryset.filter(category__parent__slug__iexact=value))
+        result = result.union(queryset.filter(category__parent__parent__slug__iexact=value))
+
+        return result
