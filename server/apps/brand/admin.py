4b248bb8fd21873d31df06456c8cbfbd49f06679
Tarlan Soltanov <tarlansoltanov@outlook.com>
2024-06-25 18:37:11 +0200

    Brand admin updated

--- server/apps/brand/admin.py
+++ server/apps/brand/admin.py
@@ -1,21 +1,41 @@
 from django.contrib import admin
 
 from server.apps.brand.models import Brand
+from server.apps.core.admin import ImageAdminMixin, ModelAdmin
 
 
 @admin.register(Brand)
-class BrandAdmin(admin.ModelAdmin):
-    list_display = (
-        "slug",
-        "name_az",
-        "name_ru",
-        "updated_at",
-        "created_at",
-    )
+class BrandAdmin(ImageAdminMixin, ModelAdmin):
+    """Brand Model admin configuration."""
+
+    list_display = ("name", "preview")
+
+    image_field_name = "photo"
+
+    preview_attrs = {
+        "width": "auto",
+        "height": "100px",
+        "display": "block",
+        "margin": "0 auto",
+    }
+
     search_fields = (
         "slug",
         "name_az",
         "name_ru",
     )
 
-    fieldsets = ((None, {"fields": ("photo", "name_az", "name_ru", "is_main")}),)
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": (
+                    "photo",
+                    "preview",
+                    "name_az",
+                    "name_ru",
+                    "is_main",
+                )
+            },
+        ),
+    )
